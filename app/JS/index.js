87cda1e55746b27fac1397825fa30b95b0d64001
Ariel Escobar <arielescobar531>
2023-12-16 19:22:03 -0300

    Finalización parcial SideBar

---
+++ app/JS/index.js
@@ -0,0 +1,7 @@
+function redirigirGitHub(){
+    window.open('https://github.com/arielsitocs', '_blank');
+}
+
+function redirigirLinkedIn(){
+    window.open('https://www.linkedin.com/in/ariel-escobar/');
+}
