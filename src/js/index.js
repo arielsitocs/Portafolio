05d3573e951279c3440d7c17e1dbb9f62a2067df
Ariel Escobar <arielescobar531>
2024-04-22 21:21:57 -0400

    1.05

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 var gmail = document.getElementById("gmail");
 var formulario = document.getElementById("form");
 
+/* Evento de icono de Gmail */
+
 if(gmail != null) {
     gmail.addEventListener("click", function(){
         formulario.scrollIntoView({behavior: "smooth"});
